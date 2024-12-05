848ea9dc0f2e57f27dc1965b50f42677dffcf678
NatsionalnoeDostoyanie <sanaflamenbaum@gmail.com>
2024-12-05 21:19:29 +0400

    Deleted db.sqlite3

--- src/global_utils/pagination.py
+++ src/global_utils/pagination.py
@@ -1,4 +1,3 @@
-import contextlib
 from typing import (
     Any,
     TypeVar,
@@ -8,7 +7,7 @@ from typing import (
 from django.conf import settings
 from django.db.models import Model
 from django.db.models.query import QuerySet
-from rest_framework.pagination import LimitOffsetPagination, _positive_int
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -31,7 +30,7 @@ class CustomLimitOffsetPagination(LimitOffsetPagination):
         self.count = self.get_count(queryset)
         self.offset = self.get_offset(request)
 
-        self.results_count = len(results_list := list(queryset[self.offset : self.offset + self.limit])) 
+        self.results_count = len(results_list := list(queryset[self.offset : self.offset + self.limit]))
 
         return results_list
 
